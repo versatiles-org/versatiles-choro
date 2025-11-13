009d9e3e51fdd4eeb7586d7bdea73857aa4f3f6d
Michael Kreil <github@michael-kreil.de>
2025-11-13 20:31:21 +0100

    refactor: update .prettierignore files and remove console log from convertPolygonsToVersatiles function

--- packages/engine/src/geometry/convert.ts
+++ packages/engine/src/geometry/convert.ts
@@ -8,7 +8,6 @@ export function convertPolygonsToVersatiles(input: string, output: string): Prog
 		throw new Error('Output file must have a .versatiles extension');
 	}
 	const mbtilesFile = output.replace(/\.versatiles$/, '.mbtiles');
-	console.log(`Converting polygons from ${input} to tiles at ${mbtilesFile}`);
 	return new ConcatenatedProgress([
 		() =>
 			runTippecanoe(input, mbtilesFile, {
