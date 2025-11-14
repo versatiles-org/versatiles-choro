f6d44bf7969064697eeae45f12c7c2381a37ae71
Michael Kreil <github@michael-kreil.de>
2025-11-14 22:30:12 +0100

    refactor: simplify ESLint configuration by removing unused imports and options

--- packages/workbench/eslint.config.js
+++ packages/workbench/eslint.config.js
@@ -1,31 +1,16 @@
-import prettier from 'eslint-config-prettier';
-import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import { defineConfig } from 'eslint/config';
-import globals from 'globals';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
+import parentConfig from '../../eslint.config.js';
 
 export default defineConfig(
+	...parentConfig,
 	{
 		ignores: ['.svelte-kit/**', 'build/**']
 	},
-	//includeIgnoreFile(gitignorePath),
-	js.configs.recommended,
-	...ts.configs.recommended,
 	...svelte.configs.recommended,
-	prettier,
 	...svelte.configs.prettier,
-	{
-		languageOptions: {
-			globals: { ...globals.browser, ...globals.node }
-		},
-		rules: {
-			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
-			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-			'no-undef': 'off'
-		}
-	},
 	{
 		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
 		languageOptions: {
