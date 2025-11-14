1b50112b72c52404042732690b90983b0e98ac65
Michael Kreil <github@michael-kreil.de>
2025-11-14 19:50:22 +0100

    feat: enhance createStyle function to filter layers based on type and improve style handling

--- packages/workbench/src/lib/component/map.ts
+++ packages/workbench/src/lib/component/map.ts
@@ -43,27 +43,60 @@ export function createStyle(backgroundMap: BackgroundMap | undefined): StyleSpec
 		language: 'de'
 	};
 
+	let style: StyleSpecification;
 	switch (backgroundMap) {
 		case 'Colorful':
-			return colorful(base);
+			style = colorful(base);
+			break;
 		case 'Gray':
-			return colorful({ ...base, recolor: { saturate: -1 } });
+			style = colorful({ ...base, recolor: { saturate: -1 } });
+			break;
 		case 'GrayBright':
-			return colorful({
+			style = colorful({
 				...base,
 				recolor: { saturate: -1, blendColor: '#ffffff', blend: 0.5 }
 			});
+			break;
 		case 'GrayDark':
-			return colorful({
+			style = colorful({
 				...base,
 				recolor: { saturate: -1, invertBrightness: true, blendColor: '#000000', blend: 0.5 }
 			});
+			break;
 		case undefined:
 		case 'None':
-			return { version: 8, sources: {}, layers: [] };
+			style = { version: 8, sources: {}, layers: [] };
+			break;
 		default:
 			throw new Error(`Unknown background map: ${backgroundMap}`);
 	}
+
+	style.layers = style.layers?.filter(layer => {
+		switch (layer.id.split(/[-:]/)[0]) {
+			case 'street':
+			case 'transport':
+			case 'symbol':
+			case 'poi':
+			case 'bridge':
+			case 'way':
+			case 'tunnel':
+			case 'marking':
+				return false;
+			case 'background':
+			case 'boundary':
+			case 'building':
+			case 'label':
+			case 'airport':
+			case 'site':
+			case 'land':
+			case 'water':
+				return true;
+		}
+		console.log(layer.id);
+		return true;
+	});
+
+	return style;
 }
 
 export function overlayStyles(style: StyleSpecification, overlayStyle: StyleSpecification): void {
