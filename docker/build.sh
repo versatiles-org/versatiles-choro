ae4ccfea1ef798a54ed381adf3fbdcdc11c550bd
Michael Kreil <github@michael-kreil.de>
2025-11-14 21:56:14 +0100

    refactor: replace build.sh with prepare.sh for Docker build context preparation

--- docker/build.sh
+++ docker/build.sh
@@ -3,19 +3,11 @@
 # go to the directory of this project
 cd "$(dirname "$0")/.."
 
-# Ensure that all *.br files in testdata/ are decompressed
-for file in testdata/*.br; do
-  if [ ! -f "${file%.br}" ]; then
-    echo "Decompressing $file"
-    brotli --decompress "$file"
-  fi
-done
+./docker/prepare.sh
 
 # Use Buildx with GHA-compatible cache
 docker buildx build \
   --tag versatiles/versatiles-choro:latest \
   --file docker/Dockerfile \
-  --cache-from type=gha \
-  --cache-to type=gha,mode=max \
   --load \
   .
