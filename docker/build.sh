0b538db967e9e14caaf53ff56458fedf4a0acf59
Michael Kreil <github@michael-kreil.de>
2025-11-14 21:29:10 +0100

    fix: ensure all .br files in testdata are decompressed before building Docker image

--- docker/build.sh
+++ docker/build.sh
@@ -3,5 +3,13 @@
 # go to the directory of this project
 cd "$(dirname "$0")/.."
 
+# Ensure that all *.br files in testdata/ are decompressed
+for file in testdata/*.br; do
+  if [ ! -f "${file%.br}" ]; then
+    echo "Decompressing $file"
+    brotli --decompress "$file"
+  fi
+done
+
 # Build the Docker image for VersaTiles Choro
 docker build -t versatiles/versatiles-choro:latest . -f docker/Dockerfile
