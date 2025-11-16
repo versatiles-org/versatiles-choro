e9d1f1ef2f72bbbeaf35b2cb0720b4458173d561
Michael Kreil <github@michael-kreil.de>
2025-11-16 17:28:45 +0100

    chore: remove unused map package and its related files

--- packages/map/src/index.ts
+++
@@ -1,4 +0,0 @@
-export function buildMap(mount: HTMLElement, config: string) {
-	console.log('buildMap called with config:', config);
-	mount.innerHTML = '<div style="padding:1em;color:#666">[Map Placeholder]</div>';
-}
