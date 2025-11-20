14e264889ffc7f87ce70e99c6427a852bfe6762d
Michael Kreil <github@michael-kreil.de>
2025-11-20 14:49:04 +0100

    refactor: improve path resolution and ensure TEMP_PATH directory creation

--- src/lib/filesystem/filesystem.server.ts
+++ src/lib/filesystem/filesystem.server.ts
@@ -1,6 +1,11 @@
+import { mkdirSync } from 'fs';
 import * as path from 'path';
 
-export const DATA_PATH = path.resolve(process.cwd(), process.env.DATA_PATH || '.');
+const CWD = process.cwd();
+export const DATA_PATH = path.resolve(CWD, process.env.DATA_PATH || '.');
+export const TEMP_PATH = path.resolve(CWD, process.env.TEMP_PATH || 'temp');
+mkdirSync(DATA_PATH, { recursive: true });
+mkdirSync(TEMP_PATH, { recursive: true });
 
 export function resolve(pathSegment: string): string {
 	return path.resolve(DATA_PATH, pathSegment.replace(/^\/+/, ''));
