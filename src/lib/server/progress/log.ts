1724bee0cd81b8bb9882cbd402a968b7a5f57980
Michael Kreil <github@michael-kreil.de>
2025-11-22 23:21:16 +0100

    fix: improve progress logging format and message handling

--- src/lib/server/progress/log.ts
+++ src/lib/server/progress/log.ts
@@ -12,18 +12,24 @@ export async function logProgress(progress: Progress) {
 
 	progress.onMessage((m, isError) => {
 		if (isError) hasErrors = true;
+		if (!m || m == message) return;
+		if (message) process.stdout.write('\n');
 		message = m;
 		log();
 	});
 
 	function log() {
 		const color = hasErrors ? '\x1b[31m' : '\x1b[32m';
-		let line = `${color}${message}`;
-		if (position > 0) line += `: ${position} %`;
-		process.stdout.write(`\x1b[2K\r${line}\x1b[0m\x1b[?25l`);
+		let line = position.toFixed(0) + '%';
+		line = ' '.repeat(4 - line.length) + line;
+		if (message) line += ` - ${message}`;
+		process.stdout.write(`\x1b[2K\r${color}${line}\x1b[0m\x1b[?25l`);
 	}
 
 	return new Promise<void>((resolve) => {
-		progress.onComplete(() => resolve());
+		progress.onComplete(() => {
+			process.stdout.write('\n');
+			resolve();
+		});
 	});
 }
